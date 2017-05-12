7b46be436ac1512b2336ed86b6e55f4363437cf1
Darian SOPORAN <dsoporan@e1p55.cluj.42.fr>
2017-05-12 16:00:39 +0300

    all done

--- libft/ft_strnew.c
+++ libft/ft_strnew.c
@@ -6,7 +6,7 @@
 /*   By: dsoporan <dsoporan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/12 17:35:56 by dsoporan          #+#    #+#             */
-/*   Updated: 2017/01/12 17:35:57 by dsoporan         ###   ########.fr       */
+/*   Updated: 2017/05/11 17:39:20 by dsoporan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,18 @@
 
 char	*ft_strnew(size_t size)
 {
-	char	*s;
 	size_t	i;
+	char	*str;
 
-	s = malloc(sizeof(char) * size + 1);
-	i = 0;
-	if (!s)
-		return (0);
-	while (s[i])
+	str = (char*)malloc(sizeof(char) * (size + 1));
+	if (str)
 	{
-		s[i] = '\0';
-		i++;
+		i = 0;
+		while (i <= size)
+		{
+			str[i] = '\0';
+			i++;
+		}
 	}
-	return (s);
+	return (str);
 }
